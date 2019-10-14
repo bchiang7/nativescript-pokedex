bb0bbdd1f8b08a056d3e42f0397252d12f8490b5
Brittany Chiang <brittany.chiang@gmail.com>
2019-10-14 15:12:46 -0400

    Set up template

---
+++ app/main.js
@@ -0,0 +1,20 @@
+import Vue from 'nativescript-vue';
+import App from './components/App';
+
+import VueDevtools from 'nativescript-vue-devtools';
+import store from './store';
+
+/* global TNS_ENV */
+if (TNS_ENV !== 'production') {
+  Vue.use(VueDevtools);
+}
+
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = TNS_ENV === 'production';
+
+Vue.prototype.$store = store;
+
+new Vue({
+  store,
+  render: h => h('frame', [h(App)]),
+}).$start();
